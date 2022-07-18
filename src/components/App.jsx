8e6f5b40e9a4e7212376ccdd46ace761bd569c5d
alyapolishchuk <alinochkarul081@gmail.com>
2022-07-18 20:41:42 +0300

    сomm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Profile from './Profile/Profile';
+import user from './DataJSON/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        photo={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      ,
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,31 @@
+// import user from '../DataJSON/user.json';
+// // import PropTypes from 'prop-types';
+// import { ReactDOM } from 'react';
+
+// function Statistics({ followers, views, likes }) {
+//   return (
+//     <>
+//       <li>
+//         <span class="label">{followers}</span>
+//         <span class="quantity">1000</span>
+//       </li>
+//       <li>
+//         <span class="label">{views}</span>
+//         <span class="quantity">2000</span>
+//       </li>
+//       <li>
+//         <span class="label">{likes}</span>
+//         <span class="quantity">3000</span>
+//       </li>
+//     </>
+//   );
+// }
+
+// ReactDOM.render(
+//   <Statistics
+//     followers={user.stats.followers}
+//     views={user.stats.views}
+//     likes={user.stats.likes}
+//   />,
+//   document.querySelector('#root')
+// );
